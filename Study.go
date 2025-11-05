1b5f3180af7c6d9bacdef9501cff0c079a961440
Samvel93 <samaelser@gmail.com>
2025-11-05 23:39:22 +0700

    Refactor main function to return unique elements from two slices

--- Study.go
+++ Study.go
@@ -3,19 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	var result int
-	fmt.Scan(&result)
-	if result >= 3 {
-		fmt.Println("Победа")
-	} else if result == 3 {
-		fmt.Println("Ничья")
-	} else {
-		fmt.Println("Поражение")
+	a := []int{4, 8, 15, 16, 23, 42, 4, 15}
+	b := []int{4, 11, 14, 8, 2, 2}
+	uniqA := make(map[int]bool)
+	uniqB := make(map[int]bool)
+	for _, i := range a {
+		uniqA[i] = true
 	}
+	for _, i := range b {
+		uniqB[i] = true
+	}
+	fmt.Println(uniqA, uniqB)
 
 }
 
-/*Входные данные: a = [5, 1, 2, 5], b = [4, 2, 5, 1, 1, 2]
+/*
+-Вернуть два слайса с уникальными элементами.
+-Найти пересечения значений в двух слайсах.
+-Вернуть слайс с уникальными элементами из слайсов a и b
+Входные данные: a = [5, 1, 2, 5], b = [4, 2, 5, 1, 1, 2]
 Выходные данные:
 [5, 1, 2], [4, 2, 5, 1]
 [5, 1, 2]
