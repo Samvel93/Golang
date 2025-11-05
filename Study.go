624814a69bff2110deb014376e6723cc39cd4c23
Samvel93 <samaelser@gmail.com>
2025-11-05 12:45:17 +0700

    Refactor password validation logic and update greeting message in Study.go

--- Study.go
+++ Study.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Привет, Sembo!")
+	pass := 7055
+	if pass == 7055 {
+		fmt.Scan(&pass)
+		fmt.Println("Пароль верный")
+	} else {
+		fmt.Println("Пароль неверный")
+
+	}
 }
