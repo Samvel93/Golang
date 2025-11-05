39d5b060a916392898b01d7302b6832968df721b
Samvel93 <samaelser@gmail.com>
2025-11-05 13:18:42 +0700

    Refactor main function to initialize arrays and print their values

--- Study.go
+++ Study.go
@@ -3,12 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	pass := 7055
-	if pass == 7055 {
-		fmt.Scan(&pass)
-		fmt.Println("Пароль верный")
-	} else {
-		fmt.Println("Пароль неверный")
+	a := [4]int{5, 1, 2, 5}
+	b := [6]int{4, 2, 5, 1, 1, 2}
+	var x []int = a[0:3]
+	var y []int = b[0:4]
+	var z []int = append(x, b[0])
+	fmt.Println(x, ",", y)
+	fmt.Println(x)
+	fmt.Println(z)
 
-	}
 }
+
+/*Входные данные: a = [5, 1, 2, 5], b = [4, 2, 5, 1, 1, 2]
+Выходные данные:
+[5, 1, 2], [4, 2, 5, 1]
+[5, 1, 2]
+[5, 1, 2, 4]
+*/
